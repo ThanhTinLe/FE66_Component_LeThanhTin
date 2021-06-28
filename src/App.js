7fd5a8c04cb6e6ba85bccafc977ee25081744f27
RayKhum113 <tinltse140972@gmail.com>
2021-06-28 16:29:35 +0700

    up code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './BaiTapThucHanhLayout/Header';
+import Body from './BaiTapThucHanhLayout/Body';
+import Footer from './BaiTapThucHanhLayout/Footer';
+
+function App() {
+  return (
+      <div>
+        <Header/>
+      <Body/>
+      <Footer/>
+      </div>
+  );
+}
+export default App;
